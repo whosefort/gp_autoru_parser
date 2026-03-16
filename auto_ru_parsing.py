c3cb441331fd913b4fae42364a7f6d01c3c27d98
nikitakreml <nikitakreml@gmail.com>
2026-03-16 22:02:04 +0300

    Initital commit with main parser class that grabs all the data from desired number of pages without autoscroll function

---
+++ auto_ru_parsing.py
@@ -0,0 +1,136 @@
+import logging
+import pandas as pd
+import numpy as np
+import bs4
+import requests
+import re
+import time
+from selenium import webdriver
+from random import randint
+
+class AutoRuParser:
+    def __init__(self):
+        # https://stackoverflow.com/questions/61114980/can-selenium-automation-be-used-with-bs4
+        self.logger = logging.getLogger(__name__)
+        self.driver = webdriver.Chrome()
+        self.main_page_url = "https://www.auto.ru/moskva/cars/all/?page="
+
+    def parse_car_html(self, card):
+        card_name_el  = card.find('a', class_=re.compile("ListingItemTitle__link"))
+        name, price, year, mileage, specs, city, seller, link  = ("_")*8
+        if card_name_el: name = card_name_el.get_text()
+        else: name = "_"
+
+        if card_name_el and card_name_el.has_attr("href"): link = card_name_el["href"]
+        else: link = "_"
+
+        if name=="_": return None
+
+        price_block = card.find('div', class_=re.compile(r'ListingItemUniversalPrice__title'))
+        if price_block: price_el = price_block.find('div')
+        else: price_el = "_"
+        if price_el!="_":
+            price_string = price_el.get_text()
+            price_digits = "".join(re.findall(r'\d+', price_string))
+            if len(price_digits)>0:
+                price = int(price_digits)
+            else:
+                price = "_"
+        spec_blocks = card.find_all('div', class_=re.compile(r'ListingItemUniversalSpecs__spec-'))
+        specs = []
+        for block in spec_blocks:
+            specs.append(block.get_text().replace('\xa0', ' '))
+        year_mileage_block = card.find('div', class_=re.compile(r'ListingItemUniversalCondition'))
+        if year_mileage_block:
+            year_el = year_mileage_block.find('div', class_=re.compile(r'Typography2__h5'))
+        else:
+            year_el = "_"
+        if year_el!="_":
+            year_string = year_el.get_text()
+        else:
+            year_string = "_"
+        year_digits = "".join(re.findall(r'\d+', year_string))
+        if len(year_digits)>0:
+            year = int(year_digits)
+
+        status_block = card.find('div', class_=re.compile(r'ListingItemUniversalCondition__status'))
+        if status_block:
+            mileage_el = status_block.find('div')
+        else:
+            mileage_el = "_"
+        if mileage_el!="_":
+            mileage_string = mileage_el.get_text()
+        else:
+            mileage_string = "_"
+        mileage_digits = "".join(re.findall(r'\d+', mileage_string))
+        if len(mileage_digits)>0:
+            mileage = int(mileage_digits)
+        else:
+            mileage = "_"
+
+        seller_block = card.find('div', class_=re.compile(r'ListingItemUniversalSeller__sellerName'))
+        if seller_block:
+            seller = seller_block.get_text()
+
+        city_block = card.find('span', class_=re.compile(r'MetroListPlace__regionName'))
+        if city_block:
+            city = city_block.get_text()
+
+        return {
+            "name": name,
+            "price": price,
+            "year": year,
+            "mileage": mileage,
+            "specs": specs,
+            "city": city,
+            "seller": seller,
+            "link": link
+        }
+
+
+    def get_page(self, page_num=0, first_time=True):
+        self.driver.get(self.main_page_url+str(page_num))
+        if first_time:
+            time.sleep(15)
+        else:
+            time.sleep(3)
+        page = bs4.BeautifulSoup(self.driver.page_source, "html.parser")
+        cards = page.find_all("div", class_=re.compile("ListingItemUniversal-"))
+        res = []
+        for card in cards:
+            res.append(self.parse_car_html(card))
+        return res
+
+    def save_backup(self, data, f):
+        list_to_pandas  = []
+        for i  in data:
+            for j in i:
+                list_to_pandas.append(j)
+        df = pd.DataFrame(list_to_pandas)
+        df.to_csv(f, index=False, encoding="utf-8")
+
+    def parse_to_dataframe(self, start_page, end_page):
+        final_list = []
+        final_list.append(self.get_page(start_page, True))
+        try:
+            for i in range(start_page+1, end_page+1):
+                page = self.get_page(i, False)
+                if page:
+                    final_list.append(page)
+                    self.logger.info(f"Page {i} of {end_page} successfully parsed")
+                else:
+                    self.logger.warning(f"Page {i} of {end_page} not parsed")
+                    continue
+        except Exception as e:
+            self.logger.error(f"Critical error while parsing: {e}")
+        finally:
+            self.save_backup(final_list,"cars_data.csv")
+
+
+
+
+if __name__ == "__main__":
+    a = AutoRuParser()
+    a.parse_to_dataframe(start_page=0,end_page=3)
+
+
